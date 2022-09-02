36d3828a4b623def6248ffad0788a083e54a1354
Artur Pushkov <edvardi72@gmail.com>
2022-09-02 11:39:47 +0300

    feat: added reflect-metadata

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,53 @@
-import { ApolloServer } from "apollo-server-express";
-import * as Express from "express";
-import { buildSchema, Query, Resolver } from "type-graphql";
-import { Server } from "http";
+import 'reflect-metadata';
+
+import { ApolloServer } from 'apollo-server-express';
+import * as Express from 'express';
+import { buildSchema, Query, Resolver } from 'type-graphql';
+import { Server } from 'http';
 
 @Resolver()
 class HelloWorldResolver {
   @Query(() => String)
   async hello() {
-    return "Hello world"
+    return 'Hello world';
   }
 }
 
-const signals = ["SIGINT", "SIGTERM", "SIGHUP"] as const;
+const signals = ['SIGINT', 'SIGTERM', 'SIGHUP'] as const;
 
-function gracegfulshutdown({signal, server}: {signal: typeof signals[number], server: Server}) {
+function gracegfulshutdown({
+  signal,
+  server,
+}: {
+  signal: typeof signals[number];
+  server: Server;
+}) {
   console.log(`Got signal: ${signal}`);
-   server.close();
-   process.exit(0);
+  server.close();
+  process.exit(0);
 }
 
 async function main() {
   const schema = await buildSchema({
-    resolvers: [HelloWorldResolver]
-  })
+    resolvers: [HelloWorldResolver],
+  });
   const apolloServer = new ApolloServer({
     schema,
   });
 
+  await apolloServer.start();
+
   const app = Express();
 
-  apolloServer.applyMiddleware({app});
+  apolloServer.applyMiddleware({ app });
   const server = app.listen(process.env.PORT || 4000, () => {
-    console.log("Server started on http://localhost:4000");
+    console.log('Server started on http://localhost:4000/graphql');
   });
 
   for (let i = 0; i < signals.length; i++) {
     process.on(signals[i], () => {
-      gracegfulshutdown({signal: signals[i], server})
-    })
+      gracegfulshutdown({ signal: signals[i], server });
+    });
   }
 }
 
